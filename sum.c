f97474f52f311bea0c8fa40389561d1f2927e794
rignyabandaru <101688499+rignyabandaru@users.noreply.github.com>
2022-05-11 09:26:39 +0530

    Add files via upload

---
+++ sum.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+main()
+{int a,b,c;
+printf("engtre the value for a:");
+scanf("%d",&a);
+printf("entre the value for b:");
+scanf("%d",&b);
+c=a+b;
+printf("the sum is :%d",c);
+}
